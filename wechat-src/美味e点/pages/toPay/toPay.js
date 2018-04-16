9def76c293ade1c3c25d146595286f7d517f05db
IamLIFI <lifi13719174004@gmail.com>
2018-04-16 11:46:46 +0800

    修改订单详情页面
    修改页面名称和完成部分要求

---
+++ wechat-src/美味e点/pages/toPay/toPay.js
@@ -0,0 +1,28 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    deskID: 10,
+    moneyToPay: 100,
+    recipeList:[
+        {
+            foodImgUrl: '../../image/food.png',
+            detail: "湛江烧耗",
+            count: 1,
+        },
+        {
+            foodImgUrl: '../../image/food.png',
+            detail: "湛江烧耗",
+            count: 2,
+        },
+        {
+            foodImgUrl: '../../image/food.png',
+            detail: "湛江烧耗",
+            count: 3,
+        }
+    ]
+  }
+ 
+})
